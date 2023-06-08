840875c67b9c65551a5b73be65a818c6d4a75a47
antoineelikem <antoinedzisam@yahoo.com>
2023-06-08 19:15:51 -0400

    3-getbookbyauthor

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,13 +20,43 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //extract the ISBN from the route paramert
+  const isbn = req.params.isbn;
+
+  //find teh book with the matching ISBN
+  const book = Object.values(books).find(book => book.isbn === isbn);
+
+  if (!book){
+      return res.status(404).json({ message: 'Book not found.'});
+  }
+  //if book was found return it with status code
+  return res.status(200).json({book: book});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  //get all keys from the books object
+  const keys = Object.keys(books);
+
+  //array to hold books by specified author
+  let booksByAuthor = [];
+
+  //Iterate through the 'books' object
+  for (let i = 0 ; i < keys.length; i++){
+      if (books[keys[i]].author === author){
+          booksByAuthor.push(books[keys[i]]);
+      }
+  }
+
+  //if no books were found
+  if(booksByAuthor.length === 0){
+      return res.status(404).json({message: 'No books found by this author.'});
+  }
+
+  return res.status(200).json({books: booksByAuthor});
 });
 
 // Get all books based on title
