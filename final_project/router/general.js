e5cc1e7b8ddd9e985989116f677c3bf158f1ce39
antoineelikem <antoinedzisam@yahoo.com>
2023-06-10 11:57:02 -0400

    finished

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,79 +31,137 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  //returns the entire books object
-  res.status(200).json({ books: books});
+  
+    // Simulate delay using promise and setTimeout
+    let myPromise = new Promise ((resolve,reject) => {
+        setTimeout(()=>{
+            resolve(books);
+        },2000);
+    });
+
+    myPromise.then(booksData => {
+        res.status(200).json({books: booksData});
+    })
+    .catch(error => {
+        res.status(500).json({message: error.message});
+    });
+
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
+  
   //extract the ISBN from the route paramert
   const isbn = req.params.isbn;
 
-  //find teh book with the matching ISBN
-  const book = Object.values(books).find(book => book.isbn === isbn);
+  //define a promise that find the book with the matching ISBN
+  let myPromise = new Promise((resolve,reject) => {
+      setTimeout(() => {
+        const book = Object.values(books).find(book => book.isbn === isbn);
 
-  if (!book){
-      return res.status(404).json({ message: 'Book not found.'});
-  }
-  //if book was found return it with status code
-  return res.status(200).json({book: book});
+        if (!book){
+            return res.status(404).json({ message: 'Book not found.'});
+        }else {
+            resolve(book);
+        }
+      }, 2000);
+  });
+  
+myPromise.then(bookData => {
+    res.status(200).json({book: bookData});
+})
+.catch(error => {
+    res.status(500).json({message: error.message});
+})
+ 
+ 
  });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const author = req.params.author;
+ public_users.get('/author/:author',function (req, res) {
+    // Extract the author from the route parameter
+    const author = req.params.author;
+  
+    // Define a Promise that finds books by the specified author
+    let myPromise = new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        // Get all keys from the books object
+        const keys = Object.keys(books);  
+  
+        // Array to hold books by specified author
+        let booksByAuthor = [];
+  
+        // Iterate through the 'books' object
+        for (let i = 0; i < keys.length; i++){
+            if (books[keys[i]].author === author){
+                booksByAuthor.push(books[keys[i]]);
+            }
+        }
+  
+        // If no books were found, reject the Promise
+        if(booksByAuthor.length === 0){
+            reject(new Error('No books found by this author.'));
+        }
+        // Otherwise, resolve the Promise with the found books
+        else {
+          resolve(booksByAuthor);
+        }
+      }, 2000);
+    });
+  
+    // Use the Promise to send a response
+    myPromise.then(booksData => {
+      res.status(200).json({books: booksData});
+    })
+    .catch(error => {
+      res.status(500).json({message: error.message});
+    });
+  });
+  
 
-  //get all keys from the books object
-  const keys = Object.keys(books);
-
-  //array to hold books by specified author
-  let booksByAuthor = [];
-
-  //Iterate through the 'books' object
-  for (let i = 0 ; i < keys.length; i++){
-      if (books[keys[i]].author === author){
-          booksByAuthor.push(books[keys[i]]);
-      }
-  }
-
-  //if no books were found
-  if(booksByAuthor.length === 0){
-      return res.status(404).json({message: 'No books found by this author.'});
-  }
-
-  return res.status(200).json({books: booksByAuthor});
-});
-
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+  public_users.get('/title/:title', function (req, res) {
     // Extract the title from the route parameter
     const title = req.params.title;
   
-    // Get all the keys for the 'books' object
-    const keys = Object.keys(books);
+    // Define a Promise that finds books by the specified title
+    let myPromise = new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        // Get all the keys for the 'books' object
+        const keys = Object.keys(books);
+    
+        // Initialize an array to hold books with the specified title
+        let booksByTitle = [];
+    
+        // Iterate through the 'books' object
+        for (let i = 0; i < keys.length; i++) {
+          // If the title of the current book matches the specified title, add the book to 'booksByTitle'
+          if (books[keys[i]].title === title) {
+            booksByTitle.push(books[keys[i]]);
+          }
+        }
+    
+        // If no books were found with the specified title, reject the Promise
+        if (booksByTitle.length === 0) {
+          reject(new Error('No books found with this title.'));
+        }
+    
+        // If books were found with the specified title, resolve the Promise with the found books
+        else {
+          resolve(booksByTitle);
+        }
+      }, 2000);
+    });
   
-    // Initialize an array to hold books with the specified title
-    let booksByTitle = [];
-  
-    // Iterate through the 'books' object
-    for (let i = 0; i < keys.length; i++) {
-      // If the title of the current book matches the specified title, add the book to 'booksByTitle'
-      if (books[keys[i]].title === title) {
-        booksByTitle.push(books[keys[i]]);
-      }
-    }
-  
-    // If no books were found with the specified title, return a 404 status code and an error message
-    if (booksByTitle.length === 0) {
-      return res.status(404).json({ message: 'No books found with this title.' });
-    }
-  
-    // If books were found with the specified title, return them with a 200 status code
-    return res.status(200).json({ books: booksByTitle });
-  });
+    // Use the Promise to send a response
+    myPromise.then(booksData => {
+      res.status(200).json({books: booksData});
+    })
+    .catch(error => {
+      res.status(500).json({message: error.message});
+    });
+});
+
   
 
 //  Get book review
